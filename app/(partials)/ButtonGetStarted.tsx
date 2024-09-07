d4acfeab5b736463ba670a7aceee3b8736fbdd87
Lovrenski <zx.nafz.id@gmail.com>
2024-09-07 19:54:38 +0700

    coloring get started button

--- app/(partials)/ButtonGetStarted.tsx
+++ app/(partials)/ButtonGetStarted.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { motion } from "framer-motion";
 import React from "react";
 import "@/assets/css/button1.css";
 
 export default function ButtonGetStarted() {
-    return (
-    <button className="button" data-text="Awesome">
-        <span className="actual-text">&nbsp;uiverse&nbsp;</span>
-        <span aria-hidden="true" className="hover-text">&nbsp;uiverse&nbsp;</span>
-    </button>
-    );
+  return (
+    <motion.button
+      initial={{ opacity: 0, y: -10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="button mt-6"
+    >
+      <span className="actual-text">&nbsp;uiverse&nbsp;</span>
+      <span aria-hidden="true" className="hover-text">
+        &nbsp;uiverse&nbsp;
+      </span>
+    </motion.button>
+  );
 }
--- app/(partials)/RetroBgGrid.tsx
+++ app/(partials)/RetroBgGrid.tsx
@@ -10,10 +10,10 @@ import DarkModeToggle from "./buttondarkmode";
 export function RetroGridDemo() {
   return (
     <div className="relative flex h-[100vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background border-none">
-      <DarkModeToggle/>
-        <HyperTextDemo />
-        <TextGenerateEffectDemo />
-        <ButtonGetStarted/>
+      <DarkModeToggle />
+      <HyperTextDemo />
+      <TextGenerateEffectDemo />
+      <ButtonGetStarted />
       <RetroGrid />
     </div>
   );
