dc59350fee1a795aba8fdb18addc82fb196b62e2
Vradita34 <vradita29@gmail.com>
2024-09-08 20:39:34 +0700

    add content and modift text reveal

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,4 +1,5 @@
 import { AppleCardsCarouselDemo } from "../(partials)/Carousel";
+import Content from "../(partials)/Content";
 import Footer from "../(partials)/Footer";
 import JoinUs from "../(partials)/JoinGallery";
 import { MarqueeDemo } from "../(partials)/MarqueeImage";
@@ -6,13 +7,14 @@ import { RetroGridDemo } from "../(partials)/RetroBgGrid";
 import { TabsDemo } from "../(partials)/Tabs";
 import { TextRevealDemo } from "../(partials)/TextReveal";
 
+
 const page = () => {
   return (
     <div className="dark:bg-black">
       <RetroGridDemo/>
-      <AppleCardsCarouselDemo />
-      <TextRevealDemo/>
       <MarqueeDemo/>
+      <TextRevealDemo/>
+      <Content/>
       <TabsDemo/>
       <JoinUs/>
       <Footer/>
--- app/(partials)/Hypertext.tsx
+++ app/(partials)/Hypertext.tsx
@@ -3,8 +3,7 @@ import HyperText from "@/components/ui/hyper-text";
 export function HyperTextDemo() {
   return (
     <HyperText
-      // className="text-5xl sm:text-8xl font-bold whitespace-pre-wrap"
-      className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-t from-[#e6f429] to-green-500 bg-clip-text text-center text-9xl font-bold leading-none tracking-tighter text-transparent"
+      className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-t from-[#e6f429] to-green-500 bg-clip-text text-center text-5xl md:text-9xl font-bold leading-none tracking-tighter text-transparent"
       text="Gallery Arts"
     />
   );
