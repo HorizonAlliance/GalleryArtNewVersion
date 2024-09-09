8eb67f02d1a31224e92f6330a0e07d3c0ed1a778
Lovrenski <zx.nafz.id@gmail.com>
2024-09-09 20:56:49 +0700

    adding transition in about page

--- app/(pages)/About/page.tsx
+++ app/(pages)/About/page.tsx
@@ -1,37 +1,75 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 type Props = {};
 
-const page = (props: Props) => {
-  return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center bg-gradient-to-b from-green-50 to-[#fbffbd]">
-      <motion.div
-        initial={{ opacity: 0, y: -10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="relative mb-4 container mx-auto px-4"
-      >
-        <h1 className="text-6xl font-bold text-center">About US</h1>
-        <h3 className="text-2xl font-bold text-center">
-          What is US? US is what?
-        </h3>
-      </motion.div>
+const AboutPage = (props: Props) => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
 
-      <motion.div
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="mt-6 w-full max-w-6xl grid grid-cols-2 gap-10"
-      >
-        <div className="flex justify-center items-center bg-teal-100 w-full h-[60vh]">
-          <div className="border-2 border-black rounded-md w-[200px] h-[400px]"></div>
-        </div>
-        <div className="bg-black w-full h-[60vh]"></div>
-      </motion.div>
-    </div>
+  const item = {
+    hidden: { opacity: 0, y: 10 },
+    show: { opacity: 1, y: 0 },
+  };
+  return (
+    <section className="flex items-center mx-auto w-full h-[100vh] px-8 bg-gradient-to-t from-green-300 to-green-50">
+      <div className="relative mx-10 px-20 w-full grid grid-cols-2">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="mx-auto w-full flex flex-col justify-center text-left px-10"
+        >
+          <motion.h1 variants={item} className="text-6xl font-bold">
+            About US
+          </motion.h1>
+          <motion.h3 variants={item} className="text-2xl font-bold">
+            What is US? US is what?
+          </motion.h3>
+          <motion.div
+            initial={{ opacity: 0, width: "0%" }}
+            animate={{ opacity: 1, width: "100%" }}
+            transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+            className="rounded-lg w-full bg-black h-[5px] mt-4"
+          ></motion.div>
+          <motion.p
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
+            className="mt-4 font-medium text-lg"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            officiis ex dolorem consequatur unde, voluptates quasi, a molestias
+            nemo suscipit error magnam, minima veniam quisquam laudantium harum
+            deserunt rerum excepturi!
+          </motion.p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1.3, ease: "easeInOut" }}
+          className="flex justify-center items-center w-full"
+        >
+          <Image
+            src="https://images.unsplash.com/photo-1725504914698-c3e97f0e9ca4?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="logo"
+            width={500}
+            height={500}
+            className="rounded-lg shadow-black shadow-md"
+          />
+        </motion.div>
+      </div>
+    </section>
   );
 };
 
-export default page;
+export default AboutPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['aceternity.com', 'assets.aceternity.com','images.unsplash.com','pbs.twimg.com'], // Add both domains here
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      "plus.unsplash.com",
+      "aceternity.com",
+      "assets.aceternity.com",
+      "images.unsplash.com",
+      "pbs.twimg.com",
+    ], // Add both domains here
+  },
+};
+
+export default nextConfig;
