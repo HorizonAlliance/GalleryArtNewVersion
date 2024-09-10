3733b1e4e6bd32fea87c1f50176a8b124570e1d2
Lovrenski <zx.nafz.id@gmail.com>
2024-09-10 21:13:21 +0700

    fixing mobile view on about page part 2

--- app/(pages)/About/page.tsx
+++ app/(pages)/About/page.tsx
@@ -25,12 +25,12 @@ const AboutPage = (props: Props) => {
   return (
     <>
       <section className="flex mx-auto w-full h-[100vh] px-8 bg-white dark:bg-black">
-        <div className="relative sm:mx-5 lg:mx-10 sm:px-8 lg:px-15 w-full grid grid-cols-2">
+        <div className="relative sm:mx-0 lg:mx-10 sm:px-8 lg:px-15 w-full grid sm:grid-rows-1 lg:grid-cols-2">
           <motion.div
             variants={container}
             initial="hidden"
             animate="show"
-            className="mx-auto w-full flex flex-col justify-center text-left sm:px-6 lg:px-10"
+            className="mx-auto w-full flex flex-col justify-center text-left sm:px-0 lg:px-10"
           >
             <motion.h1
               variants={item}
@@ -72,15 +72,16 @@ const AboutPage = (props: Props) => {
             className="flex justify-center items-center w-full"
           >
             <Image
-              src="https://images.unsplash.com/photo-1725504914698-c3e97f0e9ca4?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="https://images.unsplash.com/photo-1725681549059-77fa94863ec6?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="logo"
-              width={500}
-              height={500}
+              width={300}
+              height={300}
               className="rounded-lg shadow-black shadow-md sm:hidden lg:block"
             />
           </motion.div>
         </div>
       </section>
+
       <section className="py-24 bg-[#F5F5F5] dark:bg-gradient-to-t dark:from-[#48CFCB] dark:to-[#229799]">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mb-12">
@@ -89,13 +90,15 @@ const AboutPage = (props: Props) => {
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
               viewport={{ once: true }}
-              className="font-manrope text-5xl text-center font-bold text-gray-900 "
+              className="font-manrope text-5xl text-center font-bold text-gray-900 dark:text-white"
             >
               Our Team{" "}
             </motion.h2>
           </div>
           <motion.div
             variants={container}
+            initial="hidden"
+            animate="show"
             className="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-6 lg:grid-cols-5 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-full"
           >
             <motion.div
@@ -114,7 +117,7 @@ const AboutPage = (props: Props) => {
                   height={400}
                 />
               </div>
-              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 dark:text-white group-hover:text-indigo-600">
                 Antonio Roberto{" "}
               </h4>
               <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
@@ -137,7 +140,7 @@ const AboutPage = (props: Props) => {
                   height={400}
                 />
               </div>
-              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 dark:text-white group-hover:text-indigo-600">
                 Patricia Angely{" "}
               </h4>
               <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
@@ -164,7 +167,7 @@ const AboutPage = (props: Props) => {
                   height={400}
                 />
               </div>
-              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 dark:text-white group-hover:text-indigo-600">
                 Jerom Bell{" "}
               </h4>
               <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
@@ -187,7 +190,7 @@ const AboutPage = (props: Props) => {
                   height={400}
                 />
               </div>
-              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 dark:text-white group-hover:text-indigo-600">
                 Yasmine Tano{" "}
               </h4>
               <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
@@ -210,7 +213,7 @@ const AboutPage = (props: Props) => {
                   height={400}
                 />
               </div>
-              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+              <h4 className="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 dark:text-white group-hover:text-indigo-600">
                 Martin Darbys
               </h4>
               <span className="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
@@ -220,11 +223,18 @@ const AboutPage = (props: Props) => {
           </motion.div>
         </div>
       </section>
+
       <section className="py-24 ">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mb-10 lg:mb-16 flex justify-center items-center flex-col gap-x-0 gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between max-md:max-w-lg max-md:mx-auto">
+          <motion.div
+            initial={item.hidden}
+            whileInView={item.show}
+            transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+            viewport={{ once: true }}
+            className="mb-10 lg:mb-16 flex justify-center items-center flex-col gap-x-0 gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between max-md:max-w-lg max-md:mx-auto"
+          >
             <div className="relative w-full text-center lg:text-left lg:w-2/4">
-              <h2 className="text-4xl font-bold text-gray-900 leading-[3.25rem] lg:mb-6 mx-auto max-w-max lg:max-w-md lg:mx-0">
+              <h2 className="text-4xl font-bold text-gray-900 leading-[3.25rem] lg:mb-6 mx-auto max-w-max lg:max-w-md lg:mx-0 dark:text-white">
                 Enjoy the finest features with our products
               </h2>
             </div>
@@ -235,7 +245,7 @@ const AboutPage = (props: Props) => {
               </p>
               <a
                 href="#"
-                className="flex flex-row items-center justify-center gap-2 text-base font-semibold text-indigo-600 lg:justify-start hover:text-indigo-700 "
+                className="flex flex-row items-center justify-center gap-2 text-base font-semibold text-[#27babd] lg:justify-start hover:text-[#229799] "
               >
                 Button CTA{" "}
                 <svg
@@ -247,7 +257,7 @@ const AboutPage = (props: Props) => {
                 >
                   <path
                     d="M7.5 15L11.0858 11.4142C11.7525 10.7475 12.0858 10.4142 12.0858 10C12.0858 9.58579 11.7525 9.25245 11.0858 8.58579L7.5 5"
-                    stroke="#4F46E5"
+                    stroke="#229799"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -255,9 +265,15 @@ const AboutPage = (props: Props) => {
                 </svg>
               </a>
             </div>
-          </div>
-          <div className="flex justify-center items-center  gap-x-5 gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8">
-            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600">
+          </motion.div>
+          <motion.div
+            initial={item.hidden}
+            whileInView={item.show}
+            transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+            viewport={{ once: true }}
+            className="flex justify-center items-center  gap-x-5 gap-y-8 lg:gap-y-0 flex-wrap md:flex-wrap lg:flex-nowrap lg:flex-row lg:justify-between lg:gap-x-8"
+          >
+            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-[#229799]">
               <div className="bg-white rounded-full flex justify-center items-center mb-5 w-14 h-14 ">
                 <svg
                   width="30"
@@ -268,7 +284,7 @@ const AboutPage = (props: Props) => {
                 >
                   <path
                     d="M24.7222 11.6667V7.22225C24.7222 5.99495 23.7273 5 22.5 5H4.72222C3.49492 5 2.5 5.99492 2.5 7.22222V22.7778C2.5 24.0051 3.49492 25 4.72222 25H22.5C23.7273 25 24.7222 24.005 24.7222 22.7777V17.7778M20.8333 17.7778H25.2778C26.5051 17.7778 27.5 16.7829 27.5 15.5556V13.8889C27.5 12.6616 26.5051 11.6667 25.2778 11.6667H20.8333C19.606 11.6667 18.6111 12.6616 18.6111 13.8889V15.5556C18.6111 16.7829 19.606 17.7778 20.8333 17.7778Z"
-                    stroke="#4F46E5"
+                    stroke="#229799"
                     stroke-width="2"
                   ></path>
                 </svg>
@@ -281,7 +297,7 @@ const AboutPage = (props: Props) => {
                 Related To Your Finances
               </p>
             </div>
-            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600">
+            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-[#229799]">
               <div className="bg-white rounded-full flex justify-center items-center mb-5 w-14 h-14 ">
                 <svg
                   width="30"
@@ -292,7 +308,7 @@ const AboutPage = (props: Props) => {
                 >
                   <path
                     d="M14.375 15.8571C16.1009 15.8571 17.5 14.458 17.5 12.7321C17.5 11.0062 16.1009 9.6071 14.375 9.6071C12.6491 9.6071 11.25 11.0062 11.25 12.7321C11.25 14.458 12.6491 15.8571 14.375 15.8571ZM14.375 15.8571V20.8571M3.75 13.2264V15.2343C3.75 17.6868 3.75 18.9131 4.27747 19.9685C4.80493 21.0239 5.78567 21.76 7.74715 23.2322L8.57248 23.8516C11.4626 26.0208 12.9077 27.1054 14.5753 27.1054C16.243 27.1054 17.688 26.0208 20.5782 23.8516L21.4035 23.2322C23.365 21.76 24.3457 21.0239 24.8732 19.9685C25.4006 18.9131 25.4006 17.6868 25.4006 15.2343V13.2264C25.4006 9.95932 25.4006 8.32576 24.546 7.05852C23.6913 5.79128 22.1768 5.17918 19.1477 3.95499L18.3223 3.62144C16.4724 2.87381 15.5475 2.5 14.5753 2.5C13.6032 2.5 12.6782 2.87381 10.8283 3.62144L10.003 3.95499C6.97389 5.17919 5.45934 5.79128 4.60467 7.05852C3.75 8.32576 3.75 9.95932 3.75 13.2264Z"
-                    stroke="#4F46E5"
+                    stroke="#229799"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -307,7 +323,7 @@ const AboutPage = (props: Props) => {
                 all our customers in carrying out all transactions.
               </p>
             </div>
-            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600">
+            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-[#229799]">
               <div className="bg-white rounded-full flex justify-center items-center mb-5 w-14 h-14 ">
                 <svg
                   width="30"
@@ -318,7 +334,7 @@ const AboutPage = (props: Props) => {
                 >
                   <path
                     d="M15.0067 10V15.6652C15.0067 16.0358 15.1712 16.3873 15.4556 16.6248L18.75 19.375M15 27.5C8.09644 27.5 2.5 21.9036 2.5 15C2.5 8.09644 8.09644 2.5 15 2.5C21.9036 2.5 27.5 8.09644 27.5 15C27.5 21.9036 21.9036 27.5 15 27.5Z"
-                    stroke="#4F46E5"
+                    stroke="#229799"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -333,7 +349,7 @@ const AboutPage = (props: Props) => {
                 Hours A Week
               </p>
             </div>
-            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-indigo-600">
+            <div className="group relative w-full bg-gray-100 rounded-2xl p-4 transition-all duration-500 max-md:max-w-md max-md:mx-auto md:w-2/5 md:h-64 xl:p-7 xl:w-1/4 hover:bg-[#229799]">
               <div className="bg-white rounded-full flex justify-center items-center mb-5 w-14 h-14 ">
                 <svg
                   width="30"
@@ -344,7 +360,7 @@ const AboutPage = (props: Props) => {
                 >
                   <path
                     d="M10 14.7875L13.0959 17.8834C13.3399 18.1274 13.7353 18.1275 13.9794 17.8838L20.625 11.25M15 27.5C8.09644 27.5 2.5 21.9036 2.5 15C2.5 8.09644 8.09644 2.5 15 2.5C21.9036 2.5 27.5 8.09644 27.5 15C27.5 21.9036 21.9036 27.5 15 27.5Z"
-                    stroke="#4F46E5"
+                    stroke="#229799"
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -359,9 +375,52 @@ const AboutPage = (props: Props) => {
                 arrives and is received faster.
               </p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </section>
+
+      <section className="py-16">
+        <motion.div
+          initial={item.hidden}
+          whileInView={item.show}
+          transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
+          viewport={{ once: true }}
+          className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+        >
+          <div className="lg:py-14 lg:px-20 p-10 rounded-2xl bg-gradient-to-r from-[#48CFCB] to-[#229799] flex items-center justify-between flex-col lg:flex-row">
+            <div className="block text-center mb-5 lg:text-left lg:mb-0">
+              <h2 className="font-manrope text-4xl text-white font-semibold mb-5 lg:mb-2">
+                Connect with us
+              </h2>
+              <p className="text-xl text-indigo-100">
+                Contact us with any query or any idea.
+              </p>
+            </div>
+            <a
+              href="#"
+              className="flex items-center gap-2 bg-white rounded-full shadow-sm text-lg text-[#229799] font-semibold py-4 px-8 transition-all duration-500"
+            >
+              Get In Touch
+              <svg
+                width="19"
+                height="14"
+                viewBox="0 0 19 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.75 7L16.4167 7M11.8333 12.5L16.6852 7.64818C16.9907 7.34263 17.1435 7.18985 17.1435 7C17.1435 6.81015 16.9907 6.65737 16.6852 6.35182L11.8333 1.5"
+                  stroke="#229799"
+                  stroke-width="2.4"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </a>
+          </div>
+        </motion.div>
+      </section>
+
       <Footer />
     </>
   );
