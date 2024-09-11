521aa8efdb0565b2d9f6c72f40d91d21e8e87f77
Miyo Suzuya <damchongkun@gmail.com>
2024-09-11 19:32:36 +0700

    add content

--- app/(partials)/Card.tsx
+++ app/(partials)/Card.tsx
@@ -99,7 +99,7 @@ export const cards: Card[] = [
     title: "Amber nature water architecture",
   },
   {
-    id: 7,
+    id: 6,
     content: (
       <div>
         <p className="font-bold md:text-4xl text-xl text-white">
@@ -117,7 +117,214 @@ export const cards: Card[] = [
     title: "Background bohemian painting boho",
   },
   {
-    id: 6,
+    id: 7,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Background texture abstract art
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by ksushlapush
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Abstract",
+    thumbnail: "/assets/img/Background texture abstract art - ksushlapush.jpg",
+    title: "Background texture abstract art",
+  },
+  {
+    id: 8,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Bird abstract drawing background
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by BiancaVanDijk
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Animal",
+    thumbnail:
+      "/assets/img/Bird abstract drawing background - BiancaVanDijk.jpg",
+    title: "Bird abstract drawing background",
+  },
+  {
+    id: 10,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Flowers plants leaves floral
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by Regencygirl123
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Abstract",
+    thumbnail: "/assets/img/Flowers plants leaves floral - Regencygirl123.jpg",
+    title: "Flowers plants leaves floral",
+  },
+  {
+    id: 11,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Generated woman child the painting
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by RosesStreet
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "AI",
+    thumbnail:
+      "/assets/img/Generated woman child the painting - Roses Street.jpg",
+    title: "Generated woman child the painting",
+  },
+  {
+    id: 12,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Boho arts sunset mountains sunrise
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by Regencygirl123
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Panorama",
+    thumbnail:
+      "/assets/img/Boho arts sunset mountains sunrise - Regencygirl123.jpg",
+    title: "Boho arts sunset mountains sunrise",
+  },
+  {
+    id: 13,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Landscape sun sunset mountain
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by Regencygirl123
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Panorama",
+    thumbnail: "/assets/img/Landscape sun sunset mountain - Regencygirl123.jpg",
+    title: "Landscape sun sunset mountain",
+  },
+  {
+    id: 14,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Marble stone abstract pattern
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by Yourialka
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Abstract",
+    thumbnail: "/assets/img/Marble stone abstract pattern- Yourialka.jpg",
+    title: "Marble stone abstract pattern",
+  },
+  {
+    id: 15,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Moon sailor night shine sky see
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by betidraws
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Moon",
+    thumbnail: "/assets/img/Moon sailor night shine sky see - betidraws.jpg",
+    title: "Moon sailor night shine sky see",
+  },
+  {
+    id: 16,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Moon stars night dream landscape
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by betidraws
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Moon",
+    thumbnail: "/assets/img/Moon stars night dream landscape - Kinkate.jpg",
+    title: "Moon stars night dream landscape",
+  },
+  {
+    id: 17,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Sunrice sunset desert wall art
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by TianaZZ
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Panorama",
+    thumbnail: "/assets/img/Sunrice sunset desert wall art - TianaZZ.jpg",
+    title: "Sunrice sunset desert wall art",
+  },
+  {
+    id: 18,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Wolf moon tree silhouettes howl
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by Pezibear
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Moon",
+    thumbnail: "/assets/img/Wolf moon tree silhouettes howl - Pezibear.jpg",
+    title: "Wolf moon tree silhouettes howl",
+  },
+  {
+    id: 19,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Woman beauty portrait face
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by blauthbianca
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Person",
+    thumbnail: "/assets/img/Woman beauty portrait face - blauthbianca.png",
+    title: "Woman beauty portrait face",
+  },
+  {
+    id: 20,
     content: (
       <div>
         <p className="font-bold md:text-4xl text-xl text-white">
@@ -132,5 +339,22 @@ export const cards: Card[] = [
     category: "Abstract",
     thumbnail: "/assets/img/Background abstract wallpaper - Annomaria.jpg",
     title: "Background abstract wallpaper",
+  },
+  {
+    id: 21,
+    content: (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">
+          Boho art minimalism nature
+        </p>
+        <p className="font-normal text-base max-w-lg text-neutral-200">
+          Created by TianaZZ
+        </p>
+      </div>
+    ),
+    className: "col-span-1",
+    category: "Abstract",
+    thumbnail: "/assets/img/Boho art minimalism nature - TianaZZ.jpg",
+    title: "Boho art minimalism nature",
   },
 ];
